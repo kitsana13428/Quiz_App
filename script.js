32a7657f2c7b5a717f7f55434bc00e591094f748
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-09-01 20:54:09 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const quizData = [
+    {
+        question: "1 + 1 = ?",
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        correct: "b",
+    }, 
+    {
+        question: "1 - 1 = ?",
+        a: "3",
+        b: "2",
+        c: "1",
+        d: "0",
+        correct: "d",
+    },
+    {
+        question: "1 x 1 = ?",
+        a: "10",
+        b: "100",
+        c: "1",
+        d: "1000",
+        correct: "c",
+    },
+    {
+        question: ""
+    }
+]
