39c2df09b8953be8fcd19d0bdc9f3ed75bb335a4
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-09-02 16:02:10 +0700

    Functions Answer

--- script.js
+++ script.js
@@ -32,3 +32,57 @@ const quizData = [
         correct: "b"
     }
 ]
+
+const questionEl = document.getElementById("question"); /* สร้างตัวแปร คำตอบ */
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+
+function getSelected() {
+    const answersEls = document.querySelectorAll(".answer");
+  
+
+    answersEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            return  answerEl.id;
+        }
+    });
+
+    return undefined;
+}
+
+submitBtn.addEventListener("click", () => {
+    //เข็คคำตอบ
+    const answer = getSelected();
+    
+    currentQuiz++;
+
+    
+
+    if(answer){
+        if (currentQuiz < quizData.length){ //เมื่อคำถามหมด = เสร็จสิ้น
+            loadQuiz();
+        } else {
+            alert("You Finish!!");
+        }
+    }
+
+});
